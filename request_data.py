68a42efae29061f338d0fcbee3779c66e9ee22c6
Vladislav Mykhailoskyi <vladklp22@gmail.com>
2023-06-19 15:49:10 +0300

    chore: add request_data

---
+++ request_data.py
@@ -0,0 +1,23 @@
+headers = {
+    "authority": "api.cryptorank.io",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7",
+    "content-type": "application/json",
+    "origin": "https://cryptorank.io",
+    "referer": "https://cryptorank.io/",
+    "sec-ch-ua": '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Linux"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+}
+
+json_data = {
+    "path": "round/active",
+    "limit": 99999,
+    "filters": {},
+    "skip": 0,
+    "status": "active",
+}
